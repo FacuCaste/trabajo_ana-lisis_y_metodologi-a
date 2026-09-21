d0251af92aafa14007aa22f894acb4c77b392ca6
Facundo Castelluccio <facundo.castellucio@davinci.edu.ar>
2026-09-21 16:59:32 -0300

    feat: agrego clase Producto

--- clases.py
+++ clases.py
@@ -18,3 +18,39 @@ class Cotizacion:
         self._valor_dolar = nuevo_valor
         self._fecha_hora = datetime.now()
         print(f"Cotizacion actualizada a ${nuevo_valor} ARS por {usuario}")
+        
+class Producto:
+    def __init__(self, codigo, descripcion, precio_usd, porcentaje_margen, stock):
+        if precio_usd <= 0:
+            print("Error: El precio en USD debe ser mayor a cero.")
+        else:
+            self._precio_usd = precio_usd
+        
+        self._codigo= codigo
+        self._descripcion = descripcion
+        self._porcentaje_margen = porcentaje_margen
+        self._stock = stock
+    
+    def get_codigo(self):
+        return self._codigo
+
+    def get_descripcion(self):
+        return self._descripcion
+
+    def get_precio_usd(self):
+        return self._precio_usd
+
+    def get_stock(self):
+        return self._stock
+    
+    def calcular_precio_ars(self, cotizacion_actual):
+        precio_con_margen = self._precio_usd * (1 + (self._porcentaje_margen/100))
+        precio_final_ars = precio_con_margen * cotizacion_actual.get_valor_dolar()
+        return round(precio_final_ars,2)
+        
+    def descontar_stock(self, cantidad):
+        if cantidad < self._stock:
+            print(f"Error. Stock insuficiente para {self._descripcion}. Disponible: {self._stock}")
+        
+        self._stock -= cantidad
+        return True
